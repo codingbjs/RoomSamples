196c2ad1d52051562395cb91d8d3410a0d9fb2bf
CDBJS <codingbjs@gmail.com>
2022-02-09 07:02:30 +0900

    Diagram 생성

--- app/src/main/java/com/codingbjs/roomsamples/MainActivity.java
+++ app/src/main/java/com/codingbjs/roomsamples/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.codingbjs.roomsamples.databinding.ActivityMainBinding;
 
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mainBinding;
 
-
+    AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +20,18 @@ public class MainActivity extends AppCompatActivity {
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
 
+        db = Room.databaseBuilder(this, AppDatabase.class, "todo_db")
+                .allowMainThreadQueries()
+                .build();
 
+        mainBinding.resultText.setText(db.todoDao().getAll().toString());
 
-
+        mainBinding.addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.todoDao().insert(new Todo(mainBinding.todoEditText.getText().toString()));
+                mainBinding.resultText.setText(db.todoDao().getAll().toString());
+            }
+        });
     }
 }
